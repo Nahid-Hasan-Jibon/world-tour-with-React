6181ce62739084f470bc52c186ab1d4a9ff77fa2
Nahid-Hasan-Jibon <smnahidhasanjibon@gmail.com>
2025-01-05 23:46:50 +0600

    data fetch done

---
+++ src/components/Countries/Countries.jsx
@@ -0,0 +1,20 @@
+import { useEffect } from "react";
+import { useState } from "react";
+
+const Countries = () => {
+  const [countries, setCountries] = useState([]);
+  console.log(countries)
+
+  useEffect(()=>{
+    fetch('https://restcountries.com/v3.1/all')
+    .then(res=>res.json())
+    .then(data=>setCountries(data))
+  },[])
+  return (
+    <div>
+      <h1>Countries</h1>
+    </div>
+  );
+};
+
+export default Countries;
